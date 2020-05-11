a24bb03ba3ee0f97a8a4cb1b4ef01c52cd26928e
Steve Oney <swloney@gmail.com>
2020-05-11 10:14:52 -0400

    more cv styling

--- src/pages/oney_cv.tsx
+++ src/pages/oney_cv.tsx
@@ -10,7 +10,7 @@ export default class extends React.Component<IndexPageProps, {}> {
         super(props, context);
     }
     public render() {
-        return <div className="container">
+        return <div className="cv container">
                 <Helmet>
                     <meta charSet="utf-8" />
                     <title>Steve Oney &ndash; Curriculum Vitae</title>
@@ -119,6 +119,99 @@ export default class extends React.Component<IndexPageProps, {}> {
                         <div className="col main">
                             <div className="experience-organization">School of Information, University of Michigan</div>
                             <div className="experience-description">Presidents Post-Doctoral Fellow</div>
+                        </div>
+                    </div>
+                    <div className="row item">
+                        <div className="col side">
+                            <div className="date-range">09/2008 &ndash; 04/2015</div>
+                            <div className="location">Pittsburgh, PA</div>
+                        </div>
+                        <div className="col main">
+                            <div className="experience-organization">Carnegie Mellon University (Human-Computer Interaction Institute)</div>
+                            <div className="experience-description">Graduate student and researcher</div>
+                        </div>
+                    </div>
+                    <div className="row item">
+                        <div className="col side">
+                            <div className="date-range">03/2013 &ndash; 06/2013</div>
+                            <div className="location">San Francisco, CA</div>
+                        </div>
+                        <div className="col main">
+                            <div className="experience-organization">Advanced Technologies Labs, Adobe Systems, Research Intern</div>
+                            <div className="experience-description">Research Intern</div>
+                        </div>
+                    </div>
+                    <div className="row item">
+                        <div className="col side">
+                            <div className="date-range">06/2011 &ndash; 09/2011</div>
+                            <div className="location">San Francisco, CA</div>
+                        </div>
+                        <div className="col main">
+                            <div className="experience-organization">Advanced Technologies Labs, Adobe Systems</div>
+                            <div className="experience-description">Research Intern</div>
+                        </div>
+                    </div>
+                    <div className="row item">
+                        <div className="col side">
+                            <div className="date-range">05/2009 &ndash; 08/2009</div>
+                            <div className="location">San Jose, CA</div>
+                        </div>
+                        <div className="col main">
+                            <div className="experience-organization">IBM Research, Alamaden, Research Intern</div>
+                            <div className="experience-description">Research Intern</div>
+                        </div>
+                    </div>
+                    <div className="row item">
+                        <div className="col side">
+                            <div className="date-range">01/2007 &ndash; 08/2008</div>
+                            <div className="location">Cambridge, MA</div>
+                        </div>
+                        <div className="col main">
+                            <div className="experience-organization">MIT Media Lab, Cognitive Machines Group</div>
+                            <div className="experience-description">Researcher (M.Eng)</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="section grants">
+                    <div className="row">
+                        <div className="col side"></div>
+                        <div className="col main section-header">
+                            <h2>Grants</h2>
+                        </div>
+                    </div>
+                    <div className="row item">
+                        <div className="col side">
+                            <div className="date">07/2019</div>
+                            <div className="award-amount">$10,875</div>
+                        </div>
+                        <div className="col main">
+                            <div className="award-title">Applying Literate Programming Approaches to Support Semantic Annotation</div>
+                            <div className="award-team">Andrea Thomer and Steve Oney</div>
+                            <div className="award-sponsor">The U-M Office of Research (UMOR)</div>
+                        </div>
+                    </div>
+                    <div className="row item">
+                        <div className="col side">
+                            <div className="date">10/2019</div>
+                            <div className="award-amount">$598,926</div>
+                        </div>
+                        <div className="col main">
+                            <div className="award-title">Scalable Remote Peer Help for Programming Education</div>
+                            <div className="award-team">Steve Oney, Paul Resnick, and Christopher Brooks</div>
+                            <div className="award-sponsor">National Science Foundation (NSF)</div>
+                            <div className="award-program">Improving Undergraduate STEM Education: Education and Human Resources (IUSE: EHR)</div>
+                        </div>
+                    </div>
+                    <div className="row item">
+                        <div className="col side">
+                            <div className="date">10/2019</div>
+                            <div className="award-amount">$598,926</div>
+                        </div>
+                        <div className="col main">
+                            <div className="award-title">Designing Scalable Help Tools for Programming Courses</div>
+                            <div className="award-team">Steve Oney</div>
+                            <div className="award-sponsor">National Science Foundation (NSF)</div>
+                            <div className="award-program">Cyber-Human Systems (CHS) CRII</div>
                         </div>
                     </div>
                 </div>
