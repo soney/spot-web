4ae96269146cfa69f4272c4d9d26d71491eab79f
Steve Oney <swloney@gmail.com>
2020-12-20 18:18:23 -0500

    Updates

---
+++ component---src-pages-404-tsx-c7d9171de7613dd5e08f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n("Wbzz");t.default=function(){return a.createElement("div",null,a.createElement("h1",null,"NOT FOUND"),a.createElement("p",null,"You just hit a route that doesn't exist."),a.createElement(o.a,{to:"/"},"(go back to the homepage)"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-c7d9171de7613dd5e08f.js.map
